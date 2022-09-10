9a43ddb01a64024204f483fab67df099a2932c3b
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-09-11 01:59:49 +0500

    LinkedList using JavaScript

---
+++ main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import "./JS/LinkedList";
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://github.com/mdZaid165201b" target="_blank">
+      <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" class="logo" alt="GitHub logo" />
+    </a>
+    <h1>Hi! I am Muhammad Zaid!</h1>
+    <p class="read-the-docs">
+      Click on the GitHub logo to explore my GitHub Account just for fun
+    </p>
+  </div>
+`
+
+
+// setupCounter(document.querySelector('#counter'))
