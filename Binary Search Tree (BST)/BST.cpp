7135b96ad3838a15a81cb7d3e6dfacdca9bbcb04
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-22 16:40:59 +0500

    Update BST.cpp

--- Binary Search Tree (BST)/BST.cpp
+++ Binary Search Tree (BST)/BST.cpp
@@ -231,6 +231,34 @@ class BST{
         }
     }
     
+    int findNearest(int val){
+        int count = size(root);
+        int *arr = new int(count);
+        utilFunc(root,arr,0);
+    
+        int leastDiff = count;
+        int index = count;
+        for(int i = 0; i < count; i++){
+           if(arr[i] > val){
+               int temp = arr[i] - val;
+               if(leastDiff > temp){
+                   leastDiff = temp;
+                   index = i;
+               }
+           } 
+           else{
+               int temp = val - arr[i];
+               if(leastDiff > temp){
+                   leastDiff = temp;
+                   index = i;
+               }
+           }
+        }
+        // cout<<index;
+        // cout<<leastDiff;
+        return arr[index];
+    }
+    
 };
 
 
